d40a7f83b58487b73906e651889b23ca5e96dd45
Nurbara <iputmadep@gmail.com>
2022-04-12 05:39:55 +0700

    Bara-Md

---
+++ config.js
@@ -0,0 +1,52 @@
+/**
+   * Error Chat Gua !!!
+   wa.me/67572919291
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://api-alphabot.herokuapp.com': 'Alphabot',
+}
+
+// Other
+global.owner = ['6283174508962','6287885482936']
+global.web = 'https://youtube.com/channel/UCSitKGx6JsdfY0hJM2VAInQ'
+global.bot = 'Caung-Md'
+global.premium = ['6283174508962','6287885482936']
+global.packname = '‎‎‎‎'
+global.author = '‎'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./media/kahfz.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
